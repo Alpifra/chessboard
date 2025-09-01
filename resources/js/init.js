4f3fdf5f3f3adf0ddee619f14d817833d477ac6a
AC Développement <freelance@alexandrechauvin.fr>
2025-09-01 20:22:07 +0200

    add init pieces position from position.json

--- resources/js/init.js
+++ resources/js/init.js
@@ -1,6 +1,7 @@
 import { Board } from './Component/Board';
 import { Piece } from './Component/Piece'
 import { Game } from './Rules/Game'
+import position from './position.json';
 
 const setup = document.getElementById('setup')
 
@@ -9,7 +10,6 @@ setup.addEventListener('submit', (ev) => {
     const formData = new FormData(setup)
     const xSquares = parseInt(formData.get('size'))
     const ySquares = xSquares
-    const squares = xSquares * ySquares
     const board_container = document.querySelector('.board-container')
     const canvas = document.getElementById('chessboard')
 
@@ -17,75 +17,23 @@ setup.addEventListener('submit', (ev) => {
     board_container.classList.remove('d-none')
     setup.classList.add('d-none')
 
-    //init board
+    // init board
     let board = new Board(canvas, xSquares, ySquares)
     board.createSquares()
 
-    for (let square = 1; square <= squares; square++) {
+    // init black pieces
+    position.black.pieces.map( (p) => {
+        let piece = new Piece(p.name, board, p.square, 'black')
+        board.pieces.push(piece)
+        piece.render(board_container)
+    })
 
-        if ( square === 1 || square === 8 ) {
-            let piece = new Piece('rook', board, square, 'black')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 2 || square === 7 ) {
-            let piece = new Piece('bishop', board, square, 'black')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 3 || square === 6 ) {
-            let piece = new Piece('knight', board, square, 'black')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 4 ) {
-            let piece = new Piece('queen', board, square, 'black')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 5 ) {
-            let piece = new Piece('king', board, square, 'black')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( 9 <= square && square <= 16 ) {
-            let piece = new Piece('pawn', board, square, 'black')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( 49 <= square && square <= 56 ) {
-            let piece = new Piece('pawn', board, square, 'white')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 57 || square === 64 ) {
-            let piece = new Piece('rook', board, square, 'white')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 58 || square === 63 ) {
-            let piece = new Piece('bishop', board, square, 'white')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 59 || square === 62 ) {
-            let piece = new Piece('knight', board, square, 'white')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 60 ) {
-            let piece = new Piece('queen', board, square, 'white')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        } else if ( square === 61 ) {
-            let piece = new Piece('king', board, square, 'white')
-            board.pieces.push(piece)
-            piece.render(board_container)
-            continue
-        }
-
-    }
+    // init white pieces
+    position.white.pieces.map( (p) => {
+        let piece = new Piece(p.name, board, p.square, 'white')
+        board.pieces.push(piece)
+        piece.render(board_container)
+    })
 
     new Game(board).init()
 
